26302b2e8b82ac3cf87fe39d283dae499348b8a0
Damian Bushong <katana@codebite.net>
2013-01-25 10:18:29 -0600

    fix test script

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var blip = require('./../lib/blip'),
 blip.toImage(fs.readFileSync('input.txt'), 30, 'test.png', function(err) {
 	// no choice for now, have to do this as png
 	if(err) throw err
-)}
+})
 blip.fromImage('test.png', function(err, res) {
 	if(err) throw err
 
